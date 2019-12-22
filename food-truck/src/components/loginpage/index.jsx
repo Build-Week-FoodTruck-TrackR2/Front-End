08dde94cfdcf0af22fdf950217942dba6fa356db
Aaron Merrifield <52682445+Aaroneld@users.noreply.github.com>
2019-12-22 13:49:15 -0500

    Update index.jsx

--- food-truck/src/components/loginpage/index.jsx
+++ food-truck/src/components/loginpage/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 
 const LoginPage = (props) => {
 
     return(<div>
         <p>hello</p>
+            <Link to="/dinerdashboard" />
     </div>);
 };
 
